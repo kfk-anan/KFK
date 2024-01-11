d8d8284acf50c5469c2c331e5211ad00fedc0311
Khondoker Fuad Kabir <43852121+kfk-anan@users.noreply.github.com>
2024-01-11 22:21:40 +0600

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Creator"],
+        strings: ["Computer Science Engineer", "Computer Science Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
