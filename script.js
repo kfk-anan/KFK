9f4380a6b4d4b3264ce0d3716a9db4b4f70de709
Khondoker Fuad Kabir <43852121+kfk-anan@users.noreply.github.com>
2024-01-12 11:39:48 +0600

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Creator"],
+        strings: ["Computer Science Engineer", "Computer Science Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
